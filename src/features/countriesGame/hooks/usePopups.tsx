42dc14b8269685f08bf1a5d941e6d84415ad7466
Dima <skydmytro@gmail.com>
2024-12-06 13:50:59 +0100

    fix: popups fix

--- src/features/countriesGame/hooks/usePopups.tsx
+++ src/features/countriesGame/hooks/usePopups.tsx
@@ -1,13 +1,7 @@
-import { useEffect } from 'react';
-
 import { usePopUpStore } from '../stores/usePopUpStore';
 
 export const usePopups = () => {
-  const { closePopup, isOpened, openPopup, setPopup, currentPopup } =
-    usePopUpStore();
-  let isLosePopupOpen = false;
-  let isVictoryPopupOpen = false;
-  let isHelpPopupOpen = false;
+  const { closePopup, openPopup, setPopup } = usePopUpStore();
   const handleOpenPopup = (popupType: 'victory' | 'lose' | 'help') => () => {
     setPopup(popupType);
     openPopup();
@@ -15,18 +9,9 @@ export const usePopups = () => {
   const handleClosePopup = () => {
     closePopup();
   };
-  useEffect(() => {
-    isLosePopupOpen = currentPopup === 'lose';
-    isVictoryPopupOpen = currentPopup === 'victory';
-    isHelpPopupOpen = currentPopup === 'help';
-  }, [currentPopup]);
 
   return {
-    isOpened,
     handleOpenPopup,
     handleClosePopup,
-    isLosePopupOpen,
-    isVictoryPopupOpen,
-    isHelpPopupOpen,
   };
 };
--- src/features/countriesGame/stores/usePopUpStore.ts
+++ src/features/countriesGame/stores/usePopUpStore.ts
@@ -9,6 +9,7 @@ export type PopUpActions = {
   openPopup: () => void;
   closePopup: () => void;
   setPopup: (popup: 'victory' | 'lose' | 'help') => void;
+  openPopupByType: (popupType: 'victory' | 'lose' | 'help') => void;
 };
 
 export type PopUpStore = PopUpState & PopUpActions;
@@ -17,6 +18,8 @@ export const usePopUpStore = create<PopUpStore>((set) => ({
   isOpened: false,
   currentPopup: 'help',
   openPopup: () => set({ isOpened: true }),
+  openPopupByType: (popupType: 'victory' | 'lose' | 'help') =>
+    set({ isOpened: true, currentPopup: popupType }),
   closePopup: () => set({ isOpened: false }),
   setPopup: (popup) => set({ currentPopup: popup }),
 }));
