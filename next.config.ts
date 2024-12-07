92c5d8c5ac304c0c502b8d1b5e014c1d5f97727e
Dima <skydmytro@gmail.com>
2024-12-07 15:06:49 +0100

    feat: change the home page

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  images: {
+    domains: ['cdn.discordapp.com'],
+  },
+
   /* config options here */
 };
 
--- src/app/countries/page.tsx
+++ src/app/countries/page.tsx
@@ -6,7 +6,7 @@ import { CountryType } from '@/features/countriesGame/types/countryType';
 import { AMOUNT_OF_COUNTRIES } from '@/features/countriesGame/utils/constants';
 
 export const CountryGuesser = async () => {
-  const countryId = Math.floor(Math.random() * AMOUNT_OF_COUNTRIES);
+  const countryId = Math.floor(Math.random() * AMOUNT_OF_COUNTRIES) + 1;
   const country: CountryType = await getContryByIdRequest(countryId);
 
   return <CountryGuesserPage countryName={country.countryName} />;
